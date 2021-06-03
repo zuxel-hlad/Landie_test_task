855de0923be6a56c13f8c737528621fa9eccdf35
Stanislav <stanislavschelokov@gmail.com>
2021-06-03 17:47:07 +0300

    completed development

--- dist/js/script.js
+++ dist/js/script.js
@@ -3,21 +3,26 @@ window.addEventListener('DOMContentLoaded', () => {
   const hamburger = document.querySelector('.header-burger'),
     menu = document.querySelector('.header-menu'),
     menuLink = document.querySelectorAll('.header-nav__link');
-    btnBuy = document.querySelectorAll('.header__btn-buy');
-    logo = document.querySelectorAll('.header__logo');
+  btnBuy = document.querySelectorAll('.header__btn-buy');
+  logo = document.querySelectorAll('.header__logo');
 
   const showHamburger = () => {
     menu.classList.add('header-menu_active');
-  };
-  
-  const hideHamburger = () => {
-    menu.classList.remove('header-menu_active');
+    hamburger.classList.add('header-burger_active');
   };
 
-  hamburger.addEventListener('click', () => showHamburger());
-      menu.addEventListener('click', (e) => {
-      menuLink.forEach((link) => {
-      e.target === link || btnBuy || logo ? hideHamburger() : false;
+  const hideHamburger = () => {
+    menu.classList.remove('header-menu_active');
+    hamburger.classList.remove('header-burger_active');
+  };
+
+  hamburger.addEventListener('click', (e) => showHamburger());
+
+  menu.addEventListener('click', (e) => {
+    menuLink.forEach((link) => {
+      e.target === link || btnBuy || logo || e.target !== link || btnBuy || logo
+        ? hideHamburger()
+        : false;
     });
   });
 });
